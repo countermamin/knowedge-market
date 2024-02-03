fe97674878f25bcef1c0cd375f0f376eb4a4a06c
Orzu <orzumaminov9@gmail.com>
2024-02-03 18:04:07 +0800

    opengraph fix-bug

--- pages/index.js
+++ pages/index.js
@@ -16,11 +16,6 @@ export default function HomePage({
     <>
       <defaultHead>
         <title>Главная | KnowEdge Market</title>
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://new-ecomm-front.vercel.app/" />
-        <meta property="og:title" content="Онлайн магазин электронных товаров" />
-        <meta property="og:description" content="Онлайн магазин электронных товаров - это лучшее место для покупки смартфонов, ноутбуков, планшетов, наушников, колонок и других электронных устройств по выгодным ценам. У нас есть широкий выбор брендов и моделей, быстрая доставка и гарантия качества. Закажи сейчас и получи скидку 10% на первый заказ!" />
-        <meta property="og:image" content="https://orzu-next-ecommerce.s3.amazonaws.com/1686518863232.jpeg" />
       </defaultHead>
       <Featured product={featuredProduct} />
       <NewProducts products={newProducts} wishedProducts={wishedNewProducts} />
