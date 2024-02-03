cb4ae64bbdb973c657e78e4743cd2be67ba4af06
Orzu <orzumaminov9@gmail.com>
2024-02-03 18:22:18 +0800

    OG fix

--- pages/_document.js
+++ pages/_document.js
@@ -6,13 +6,14 @@ function MyDocument(props) {
     <Html>
       <Head>
       <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="KnowEdge Market"/>
         <meta property="og:url" content="https://new-ecomm-front.vercel.app/" />
         <meta property="og:title" content="Онлайн магазин электронных товаров" />
         <meta property="og:description" content="Онлайн магазин электронных товаров - это лучшее место для покупки смартфонов, ноутбуков, планшетов, наушников, колонок и других электронных устройств по выгодным ценам. У нас есть широкий выбор брендов и моделей, быстрая доставка и гарантия качества. Закажи сейчас и получи скидку 10% на первый заказ!" />
         <meta property="og:image" content="https://orzu-next-ecommerce.s3.amazonaws.com/1686518863232.jpeg" />
       </Head>
       <body>
-        <Main />
+        <Main />ЫЫ
         <NextScript />
       </body>
     </Html>
